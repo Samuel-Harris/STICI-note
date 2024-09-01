d701bbd270fa263c8e0970de91cc68a3b5a387b0
Samuel Harris <samuel.matsuo.harris@gmail.com>
2024-09-01 18:25:34 +0100

    Removed model_loaders.py

--- evaluation/vector_db/vector_db.py
+++ evaluation/vector_db/vector_db.py
@@ -2,8 +2,6 @@ from langchain_community.vectorstores import Chroma
 from langchain_core.documents import Document
 from langchain_core.embeddings import Embeddings
 
-COLLECTION_NAME: str = "document"
-
 
 def construct_chroma_client(text_chunks: list[Document], embedding_function: Embeddings) -> Chroma:
     return Chroma.from_documents(text_chunks, embedding_function)
