006c169b44ae02f5ab9b48485166ab318b322873
Samuel Harris <samuel.matsuo.harris@gmail.com>
2024-09-01 18:58:05 +0100

    Removed unnecessary @overrides decorator from basic_pipeline.py

--- evaluation/pipelines/basic_pipeline.py
+++ evaluation/pipelines/basic_pipeline.py
@@ -4,21 +4,18 @@ from langchain.prompts import PromptTemplate
 from langchain_core.language_models import LanguageModelLike
 from langchain_core.runnables import Runnable
 from langchain_core.vectorstores import VectorStoreRetriever
-from overrides import overrides
 
-from pipelines.pipeline import Pipeline
+from pipeline import Pipeline
 
 
 class BasicPipeline(Pipeline):
     retrieval_chain: Runnable
 
-    @overrides
     def __init__(self, llm: LanguageModelLike, prompt_template: PromptTemplate,
                  vector_db_retriever: VectorStoreRetriever) -> None:
         combine_docs_chain = create_stuff_documents_chain(llm, prompt_template)
 
         self.retrieval_chain = create_retrieval_chain(vector_db_retriever, combine_docs_chain)
 
-    @overrides
     def query(self, query: str) -> str:
         return self.retrieval_chain.invoke({"input": query})
