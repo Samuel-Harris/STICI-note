a324b0f3b8e456a21a2fe2a711f987822cebf954
Samuel Harris <samuel.matsuo.harris@gmail.com>
2024-09-02 20:18:56 +0100

    Test using LlamaCpp for quantised models

--- evaluation/evaluation_suite/evaluation_suite.py
+++ evaluation/evaluation_suite/evaluation_suite.py
@@ -6,7 +6,7 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from evaluation.vector_db.vector_db import construct_chroma_client
 from evaluation.pipelines.basic_pipeline import BasicPipeline
 from evaluation.models.model_factory import construct_hf_model
-from transformers import BitsAndBytesConfig
+
 
 raw_prompt_template = """<|system|>
 In this conversation between a user and the AI, the AI is helpful and friendly, and when it does not know the answer it says "I don’t know".
@@ -19,7 +19,9 @@ To help answer the question, you can use the following information:
 """
 prompt_template = PromptTemplate(template=raw_prompt_template, input_variables=["input", "context"])
 
-model = load_llama_cpp_model("../prototype/tinyllama-1.1b-chat-v1.0.Q6_K.gguf", 0.75, 1)
+print(f"Is torch available on MPS: {torch.backends.mps.is_available()}")
+
+model = construct_hf_model("models/model_weights/tinyllama-1.1b-chat-v1.0.Q6_K.gguf")
 
 embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 text_chunks = [Document("My favourite big cat is the lion."), Document("Koalas are my favourite animal")]
--- evaluation/models/model_factory.py
+++ evaluation/models/model_factory.py
@@ -1,8 +1,19 @@
-from transformers import AutoModelForCausalLM, BitsAndBytesConfig, AutoTokenizer
+from langchain_community.llms.llamacpp import LlamaCpp
+from langchain_core.callbacks import CallbackManager, StreamingStdOutCallbackHandler
+from langchain_core.language_models import LLM
 
 
-def construct_hf_model(model_id: str, quantisation_config: BitsAndBytesConfig) -> (AutoTokenizer, AutoModelForCausalLM):
-    tokenizer = AutoTokenizer.from_pretrained(model_id)
-    model = AutoModelForCausalLM.from_pretrained(model_id, quantization_config=quantisation_config)
+def construct_hf_model(model_path: str) -> LLM:
+    callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
 
-    return tokenizer, model
+    return LlamaCpp(model_path=model_path,
+                    temperature=0.75,
+                    max_tokens=2000,
+                    top_p=1,
+                    callback_manager=callback_manager,
+                    verbose=True,
+                    n_ctx=2048,
+                    n_batch=512,
+                    n_gpu_layers=-1,
+                    f16_kv=True,
+                    )
