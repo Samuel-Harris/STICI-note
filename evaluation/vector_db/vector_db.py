60993b5ce8c6ea7d86c63f9c77b0bd3397ca1760
Samuel Harris <samuel.matsuo.harris@gmail.com>
2024-09-25 00:21:37 +0100

    Made vector_db.py typse more Chroma-specific

--- evaluation/vector_db/vector_db.py
+++ evaluation/vector_db/vector_db.py
@@ -1,7 +1,6 @@
 from langchain_chroma import Chroma
 from langchain_core.embeddings import Embeddings
-from langchain_core.vectorstores import VectorStoreRetriever
 
 
-def construct_chroma_vector_store_retriever(embedding_function: Embeddings) -> VectorStoreRetriever:
-    return Chroma(embedding_function=embedding_function).as_retriever()
+def construct_chroma_vector_store(embedding_function: Embeddings) -> Chroma:
+    return Chroma(embedding_function=embedding_function)
